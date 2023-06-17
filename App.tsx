207095a333e7d1443718071d6e6ecb6983bef5d4
Sherehime <jon_miller_j@mail.ru>
2023-06-17 23:45:00 +0300

    17 06

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Жопой гвозди выдергивать планирую</Text>
+      <Text>Жопой гвозди выдергивать планирую...</Text>
       <StatusBar style="auto" />
     </View>
   );
